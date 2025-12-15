8b0577f31ee6559d0b29a571737efdec736f0826
Wallinera <emirhan-karagoz1@hotmail.com>
2025-12-15 13:05:52 +0300

    Initial Commit

--- src-previoıs/components/UI/AppLayout.jsx
+++
@@ -1,7 +0,0 @@
-export default function AppLayout({ children }) {
-  return (
-    <div className="flex flex-col w-300 text-black my-40 rounded-xl shadow-lg border border-gray-200 p-4 pb-10">
-      {children}
-    </div>
-  );
-}
